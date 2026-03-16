c5d5193242729992261cb795de7de6dbceff9eba
gametag7892 <eduardosoaresmota3@gmail.com>
2026-03-16 20:51:09 -0300

    Encaminhando o TBL novamente

---
+++ TBL-II-P2/Models/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class PokeInfo{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Height { get; set; }
+    public string Weight { get; set; }
+
+    public string Base_Experience { get; set; }
+}
